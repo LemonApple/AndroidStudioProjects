42becb45099b618085e21002bca666a24ce1b067
陽明 <16509526+LemonApple@users.noreply.github.com>
2020-11-16 09:52:31 +0800

    Update LaunchFragment.java

--- 8_Shoppingcartimprovementandimprovementdynamicregistrationfragments/app/src/main/java/com/example/a8_shoppingcartimprovementandimprovementdynamicregistrationfragments/fragment/LaunchFragment.java
+++ 8_Shoppingcartimprovementandimprovementdynamicregistrationfragments/app/src/main/java/com/example/a8_shoppingcartimprovementandimprovementdynamicregistrationfragments/fragment/LaunchFragment.java
@@ -69,8 +69,14 @@ public class LaunchFragment extends Fragment {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(mContext, "欢迎您开启美好生活", Toast.LENGTH_SHORT).show();
-                    //Intent intent = new Intent(this, MainActivity.class);
-                    //startActivity(intent);
+                    
+/**
+返回一个和此fragment绑定的FragmentActivity或者其子类的实例。相反，如果此fragment绑定的是一个context的话，怎可能会返回null。
+	因为getActivity()大部分都是在fragment中使用到，而fragment需要依赖于activity，所有我们在fragment里头需要做一些动作，比如启动一个activity，就需要拿到activity对象才可以启动，而fragment对象是没有startActivity()方法的。
+     */
+
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
                 }
             });
         }
